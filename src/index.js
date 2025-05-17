7cbaf948daff0ecb12743fbb87559871cd0bf9c0
skyc4ffe1ne <skyc4ff3ine@virgilio.it>
2025-05-17 17:25:55 +0200

    Added more commands

--- src/index.js
+++ src/index.js
@@ -3,18 +3,44 @@ const express = require("express");
 const { isValid, format } = require("date-fns");
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
+
 const bot = new TelegramBot(token, { polling: true });
+
+const allCommands = [
+  {
+    command: "/\start",
+    description: "Start the bot",
+  },
+  {
+    command: "/\createEvent",
+    description: "Creation of the event",
+  },
+  {
+    command: "/\help",
+    description: "Showing all the commands",
+  },
+  {
+    command: "/\see",
+    description: "See all the events",
+  },
+];
 const state = {
   initialState: "default",
   makeEvent: "createEvent",
 };
+
 let currentState = state.initialState;
 
+// TODO: Write all the commands after the user type start.
 bot.onText(/\/start/, (msg) => {
   const chat_id = msg.chat.id;
   bot.sendMessage(
     chat_id,
-    "Hello, let's create a new events: <list all commands>", // TODO: Write all the commands after the user type start.
+    `Hello, let's create a *new event*:
+    /\createEvent: for create a new event
+    /\help: for list all the comands
+    /\see: for list all the events`,
+    { parse_mode: "MarkdownV2" },
   );
 });
 
@@ -22,23 +48,42 @@ bot.onText(/\/createEvent/, (msg) => {
   const chat_id = msg.chat.id;
   bot.sendMessage(
     chat_id,
-    "Let's create a new event: \n Type the date in the following format Year/Month/Day/Hours/Minutes",
+    `Type the date in the following format: 
+     *YYYY/MM/DD HH:MM*`,
+    { parse_mode: "MarkdownV2" },
   );
   currentState = state.makeEvent;
 });
 
 bot.on("message", (msg) => {
   const chat_id = msg.chat.id;
-  if (currentState === "createEvent") {
+  if (currentState === state.makeEvent) {
     const text = msg.text;
     if (isValid(new Date(text))) {
       const dateFormatted = format(text, "do 'of' MMMM yyyy 'at' HH':'mm");
-      bot.sendMessage(chat_id, `This is the day:\n${dateFormatted}`);
-      currentState = state.makeHours;
+      bot.sendMessage(
+        chat_id,
+        `This is the day:
+        *${dateFormatted}*
+        If you wanna to change it: /\change`,
+        { parse_mode: "MarkdownV2" },
+      );
     } else {
       bot.sendMessage(chat_id, "Please type a good date: Year/Month/Day");
     }
   }
+});
+
+bot.onText(/\/help/, (msg) => {
+  const chat_id = msg.chat.id;
+  bot.sendMessage(
+    chat_id,
+    `${allCommands
+      .map((el) => {
+        return el.command + ": " + el.description;
+      })
+      .join("\n")}`,
+  );
 });
 
 bot.onText(/\/time/, (msg) => {
